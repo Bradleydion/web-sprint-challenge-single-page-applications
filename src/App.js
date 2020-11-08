7295d1a98c81c175e7f93f8aa3e58ac18b1265b8
Bradley Dion <bradleydion@Bradleys-iMac.local>
2020-11-08 17:11:39 -0500

    creating my pages slowly but surely

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from "react";
+import {Navbar, Button, Container, Row, Col, Card, CardImg,CardImgOverlay, CardText,CardBody, CardLink, CardTitle} from "reactstrap";
+import PizzPhoto from "./PizzaPhoto/chad-montano-MqT0asuoIcU-unsplash.jpg"
+import {useRouteMatch} from "react-router-dom"
+
 
 const App = () => {
+
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <Container>
+      <Navbar>
+          <Row>
+            <Col sm={{size: 'auto'}}><h1>Lambda Eats</h1> 
+            </Col>
+            <Col sm={{size: 'auto'}}><Button color="primary">Home</Button></Col>
+            <Col sm={{size: 'auto'}}><Button color="secondary">About</Button></Col>
+          </Row>
+      </Navbar>
+      <Card inverse>
+        <CardImg width="100%" src={PizzPhoto}alt= "Card image cap"/>
+        <CardImgOverlay>
+         <Col>
+            <Row><CardTitle tag="h3">Your favorite food, deliverd while coding</CardTitle></Row>
+          <Row><Button color="primary">Pizza?</Button></Row>
+          </Col>
+        </CardImgOverlay>
+      </Card>
+
+    </Container>
   );
 };
 export default App;
